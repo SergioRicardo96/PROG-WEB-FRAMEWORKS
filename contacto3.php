e0692e3687b3e1d3ad459e1bf81e8463b09638da
chochis123 <60901956+chochis123@users.noreply.github.com>
2020-02-11 10:37:05 -0600

    parte3

---
+++ contacto3.php
@@ -0,0 +1,39 @@
+<?php include('header3.php');?>
+
+      <!-- Main Content -->
+      <div class="content-box">
+         <!-- Hero Section -->
+         <section class="section section-hero">
+            <div class="hero-box">
+               <div class="container">
+                  <div class="hero-text align-center">
+                     <h1>Pagina de contacto</h1>
+                  </div>
+               </div>
+            </div>
+
+         <br>  <br/>
+
+                <!-- Boats Section -->
+         <section class="section section-boats">
+            <!-- Title -->
+            <div class="section-title">
+               <div class="container">
+                  <h2 class="title">INSTITUTO TECNOLOGICO DE TUXTLA GUTIERREZ</h2>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d122257.91829279102!2d-93.21527618733505!3d16.74879468314202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ecd964bb4d07fd%3A0x4e771d3242c2f25a!2sTecnol%C3%B3gico%20Nacional%20de%20M%C3%A9xico%20Campus%20Tuxtla%20Guti%C3%A9rrez!5e0!3m2!1ses-419!2smx!4v1581387481529!5m2!1ses-419!2smx" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                  <br>  <br/>
+                  <h2 class="title">INTEGRANTES DEL EQUIPO</h2>
+                  <p class="sub-title">Gonzalez Vera Carlos Emilio</p>
+                  <p class="sub-title">Lopez Ruiz Rosa Ivette</p>
+                  <p class="sub-title">Nanguse Gonzalez Sergio Ricardo</p>
+                  <br>  <br/>
+                  <h2 class="title">NUMERO DE CONTACTO</h2>
+                  <p class="sub-title">9616161657</p>
+               </div>
+            </div>
+            
+      <!-- Scripts -->
+      <script src="js/jquery.js"></script>
+      <script src="js/functions.js"></script>
+   </body>
+</html>
